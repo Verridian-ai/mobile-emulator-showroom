13b9e9298b3257c1e0702ed4873fdfd0578db6b6
Your Name <your.email@example.com>
2025-10-01 01:59:40 +1000

    chore: remove test file for pre-commit hooks

--- test-precommit.js
+++
@@ -1,9 +0,0 @@
-// Test file for pre-commit hook validation
-
-const testFunction = () => {
-  console.log('This code has formatting issues');
-  const badIndent = true;
-  return badIndent;
-};
-
-export default testFunction;
