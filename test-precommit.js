ed208ade83ca1b248a6b894d7aa85171d8f6921b
Your Name <your.email@example.com>
2025-10-01 01:59:10 +1000

    test: verify pre-commit hooks

---
+++ test-precommit.js
@@ -0,0 +1,9 @@
+// Test file for pre-commit hook validation
+
+const testFunction = () => {
+  console.log('This code has formatting issues');
+  const badIndent = true;
+  return badIndent;
+};
+
+export default testFunction;
